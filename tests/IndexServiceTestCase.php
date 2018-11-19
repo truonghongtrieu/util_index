1d9a687e83a93b2bd5b339d7cf6d839b5c82f11f
Andy Truong <thehongtt@gmail.com>
2018-11-19 14:56:07 +1000

    Make base test case smaller by splitting helper methods.

--- tests/IndexServiceTestCase.php
+++ tests/IndexServiceTestCase.php
@@ -2,42 +2,22 @@
 
 namespace go1\util_index\tests;
 
-use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Schema\Schema as DBSchema;
-use Elasticsearch\Client;
-use Elasticsearch\Common\Exceptions\Missing404Exception;
-use go1\clients\EventClient;
-use go1\clients\LoClient;
 use go1\clients\MqClient;
-use go1\core\lo\event_li\index\tests\EventSchema;
 use go1\util\DB;
 use go1\util\es\mock\EsInstallTrait;
-use go1\util\es\Schema;
-use go1\util\schema\AssignmentSchema;
-use go1\util\schema\AwardSchema;
-use go1\util\schema\CollectionSchema;
-use go1\util\schema\ContractSchema;
-use go1\util\schema\CouponSchema;
-use go1\util\schema\CreditSchema;
-use go1\util\schema\EckSchema;
-use go1\util\schema\EnrolmentSchema;
 use go1\util\schema\InstallTrait;
-use go1\util\schema\MailSchema;
-use go1\util\schema\MetricSchema;
-use go1\util\schema\PaymentSchema;
-use go1\util\schema\PolicySchema;
-use go1\util\schema\QuizSchema;
 use go1\util\user\UserHelper;
 use go1\util_index\HistoryRepository;
 use go1\util_index\IndexSchema;
 use go1\util_index\IndexService;
-use go1\util_index\task\TaskRepository;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
 
 abstract class IndexServiceTestCase extends TestCase
 {
+    use IndexServiceTestHelperTrait;
     use InstallTrait;
     use EsInstallTrait;
 
@@ -45,11 +25,6 @@ abstract class IndexServiceTestCase extends TestCase
     protected $isUnitTestCase          = false;
     protected $messages;
     protected $logs;
-
-    protected function client(IndexService $app): Client
-    {
-        return $app['go1.client.es'];
-    }
 
     protected function getDatabases()
     {
@@ -61,13 +36,19 @@ abstract class IndexServiceTestCase extends TestCase
 
     protected function getApp(): IndexService
     {
+        putenv('ES_URL=http://localhost:9200');
+
         /** @var IndexService $app */
         $app = require __DIR__ . '/../public/index.php';
         $app['waitForCompletion'] = true;
         $app['dbs'] = $app->extend('dbs', function () { return $this->getDatabases(); });
 
         $app->extend('history.repository', function () {
-            $history = $this->getMockBuilder(HistoryRepository::class)->disableOriginalConstructor()->setMethods(['write', 'bulkLog'])->getMock();
+            $history = $this
+                ->getMockBuilder(HistoryRepository::class)
+                ->disableOriginalConstructor()
+                ->setMethods(['write', 'bulkLog'])
+                ->getMock();
 
             $history
                 ->expects($this->any())
@@ -83,16 +64,22 @@ abstract class IndexServiceTestCase extends TestCase
                 ->expects($this->any())
                 ->method('bulkLog')
                 ->willReturnCallback(function (array $response) {
-                    if ($response['errors']) {
-                        foreach ($response['items'] as $item) {
-                            foreach ($item as $action => $data) {
-                                if (isset($data['error'])) {
-                                    $this->logs[] = [
-                                        'message' => !$data['error'] ? null : (is_string($data['error']) ? $data['error'] : json_encode($data['error'])),
-                                        'status'  => $data['status'],
-                                    ];
-                                }
+                    if (!$response['errors']) {
+                        return null;
+                    }
+
+                    foreach ($response['items'] as $item) {
+                        foreach ($item as $action => $data) {
+                            if (!isset($data['error'])) {
+                                continue;
                             }
+
+                            $this->logs[] = [
+                                'message' => !$data['error']
+                                    ? null
+                                    : (is_string($data['error']) ? $data['error'] : json_encode($data['error'])),
+                                'status'  => $data['status'],
+                            ];
                         }
                     }
                 });
@@ -174,48 +161,13 @@ abstract class IndexServiceTestCase extends TestCase
         });
     }
 
-    protected function indices()
-    {
-        return [Schema::INDEX];
-    }
-
     protected function appInstall(IndexService $app)
     {
-        $app->extend('go1.client.lo', function () {
-            $client = $this
-                ->getMockBuilder(LoClient::class)
-                ->setMethods(['eventAvailableSeat'])
-                ->disableOriginalConstructor()
-                ->getMock();
-
-            $client
-                ->expects($this->any())
-                ->method('eventAvailableSeat')
-                ->willReturn(12); # The logic is not important here.
-
-            return $client;
-        });
-
-        $app->extend('go1.client.event', function () {
-            $client = $this
-                ->getMockBuilder(EventClient::class)
-                ->setMethods(['getAvailableSeats'])
-                ->disableOriginalConstructor()
-                ->getMock();
-
-            $client
-                ->expects($this->any())
-                ->method('getAvailableSeats')
-                ->willReturn(12);
-
-            return $client;
-        });
-
         $this->installGo1Schema($app['dbs']['go1'], $coreOnly = false);
+        DB::install($app['dbs']['go1'], [function (DBSchema $schema) { IndexSchema::install($schema); }]);
 
         if (!$this->isUnitTestCase) {
             $client = $this->client($app);
-
             $indices = $this->indices();
             foreach ($indices as $index) {
                 if ($client->indices()->exists(['index' => $index])) {
@@ -224,62 +176,5 @@ abstract class IndexServiceTestCase extends TestCase
             }
             $this->installEs($client, $indices);
         }
-
-        DB::install($app['dbs']['assignment'], [
-            function (DBSchema $schema) {
-                AssignmentSchema::install($schema);
-                MailSchema::install($schema);
-                EckSchema::install($schema);
-                PaymentSchema::install($schema);
-                EnrolmentSchema::installManualRecord($schema);
-                CouponSchema::install($schema);
-                CreditSchema::install($schema);
-                AwardSchema::install($schema);
-                ContractSchema::install($schema);
-                MetricSchema::install($schema);
-                IndexSchema::install($schema);
-                CollectionSchema::install($schema);
-                PolicySchema::install($schema);
-            },
-        ]);
-
-        DB::install($app['dbs']['quiz'], [
-            function (DBSchema $schema) {
-                QuizSchema::install($schema);
-            },
-        ]);
-
-        DB::install($app['dbs']['event'], [
-            function (DBSchema $schema) {
-                EventSchema::install($schema);
-            },
-        ]);
-    }
-
-    protected function db(IndexService $app): Connection
-    {
-        return $app['dbs']['default'];
-    }
-
-    protected function taskRepository(IndexService $app): TaskRepository
-    {
-        return $app['task.repository'];
-    }
-
-    protected function assertMissing404Exception(callable $function)
-    {
-        try {
-            call_user_func($function);
-            $this->fail('No exception thrown');
-        } catch (Missing404Exception $e) {
-            $this->assertTrue($e instanceof \Exception);
-        }
-    }
-
-    protected function assertDocField($value, array $hit, $field = 'id')
-    {
-        $this->assertEquals($value, $hit['_source'][$field]);
-
-        return $this;
     }
 }
---
+++ tests/IndexServiceTestHelperTrait.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace go1\util_index\tests;
+
+use Doctrine\DBAL\Connection;
+use Elasticsearch\Client;
+use Elasticsearch\Common\Exceptions\Missing404Exception;
+use Exception;
+use go1\util\es\Schema;
+use go1\util_index\IndexService;
+use go1\util_index\task\TaskRepository;
+use PHPUnit\Framework\TestCase;
+
+trait IndexServiceTestHelperTrait
+{
+    protected function indices()
+    {
+        return [Schema::INDEX];
+    }
+
+    protected function assertMissing404Exception(callable $function)
+    {
+        /** @var TestCase $this */
+        try {
+            call_user_func($function);
+            $this->fail('No exception thrown');
+        } catch (Missing404Exception $e) {
+            $this->assertTrue($e instanceof Exception);
+        }
+    }
+
+    protected function assertDocField($value, array $hit, $field = 'id')
+    {
+        /** @var TestCase $this */
+        $this->assertEquals($value, $hit['_source'][$field]);
+
+        return $this;
+    }
+
+    protected function client(IndexService $app): Client
+    {
+        return $app['go1.client.es'];
+    }
+
+    protected function db(IndexService $app): Connection
+    {
+        return $app['dbs']['default'];
+    }
+
+    protected function taskRepository(IndexService $app): TaskRepository
+    {
+        return $app['task.repository'];
+    }
+}
