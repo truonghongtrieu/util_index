0c9f1f8a70d110d5f0b39499134bb3846371d5a1
Andy Truong <thehongtt@gmail.com>
2018-12-04 22:14:06 +1000

    history wring: trigger error for testing env, trying to make sure errors are not silently ignored.

--- HistoryRepository.php
+++ HistoryRepository.php
@@ -3,6 +3,7 @@
 namespace go1\util_index;
 
 use Doctrine\DBAL\Connection;
+use PHPUnit\Framework\TestCase;
 
 class HistoryRepository
 {
@@ -22,6 +23,12 @@ class HistoryRepository
             'data'      => !$data ? null : (is_string($data) ? $data : json_encode($data)),
             'timestamp' => $timestamp ?: time(),
         ]);
+
+        if (class_exists(TestCase::class, false)) {
+            if (is_string($data)) {
+                trigger_error('error: ' . $data);
+            }
+        }
     }
 
     public function bulkLog(array $response)
