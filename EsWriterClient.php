c8e0c64e89b0635370eb3ea8c4a73367ad4ba8b7
Duy Nguyen <duy.nguyen@go1.com.au>
2018-12-18 09:24:32 +1000

    Es writer client validate payload

--- EsWriterClient.php
+++ EsWriterClient.php
@@ -2,7 +2,9 @@
 
 namespace go1\util_index;
 
+use Assert\Assert;
 use go1\clients\MqClient;
+use RuntimeException;
 
 class EsWriterClient
 {
@@ -15,8 +17,18 @@ class EsWriterClient
         $this->routingKey = $routingKey;
     }
 
+    private function validate(array $params, string $requiredFields)
+    {
+        $assertion = Assert::lazy();
+        foreach (explode(",", $requiredFields) as $requiredField) {
+            $assertion->that($params[$requiredField] ?? null, $requiredField)->notEmpty();
+        }
+        $assertion->verifyNow();
+    }
+
     public function delete($params)
     {
+        $this->validate($params, 'index,type,id,body');
         $uri = sprintf(
             '/%s/%s/%s?routing=%s',
             $params['index'],
@@ -25,26 +37,16 @@ class EsWriterClient
             $params['routing']
         );
 
-        $this->mqClient->publish([
-            'uri'         => $uri,
-            'http_method' => 'DELETE',
-        ], $this->routingKey);
+        $this->mqClient->publish(['uri' => $uri, 'http_method' => 'DELETE'], $this->routingKey);
     }
 
     public function updateByQuery($params)
     {
-        $uri = sprintf(
-            '/%s/%s/_update_by_query',
-            $params['index'],
-            $params['type']
-        );
+        $this->validate($params, 'index,type,body,routing');
 
+        $uri = sprintf('/%s/%s/_update_by_query', $params['index'], $params['type']);
         isset($params['routing']) && $uri .= sprintf('?routing=%s', $params['routing']);
-
-        $this->mqClient->publish([
-            'uri'  => $uri,
-            'body' => $params['body'],
-        ], $this->routingKey);
+        $this->mqClient->publish(['uri' => $uri, 'body' => $params['body']], $this->routingKey);
     }
 
     public function index($params)
@@ -54,6 +56,7 @@ class EsWriterClient
 
     public function create($params)
     {
+        $this->validate($params, 'index,type,id,body,routing');
         $uri = sprintf(
             '/%s/%s/%s/_create?routing=%s',
             $params['index'],
@@ -61,14 +64,12 @@ class EsWriterClient
             $params['id'],
             $params['routing']
         );
-        $this->mqClient->publish([
-            'uri'  => $uri,
-            'body' => $params['body'],
-        ], $this->routingKey);
+        $this->mqClient->publish(['uri' => $uri, 'body' => $params['body']], $this->routingKey);
     }
 
     public function update($params)
     {
+        $this->validate($params, 'index,type,id,body,routing');
         $uri = sprintf(
             '/%s/%s/%s/_update?routing=%s',
             $params['index'],
@@ -77,14 +78,13 @@ class EsWriterClient
             $params['routing']
         );
 
-        $this->mqClient->publish([
-            'uri'  => $uri,
-            'body' => $params['body'],
-        ], $this->routingKey);
+        $this->mqClient->publish(['uri' => $uri, 'body' => $params['body']], $this->routingKey);
     }
 
     public function bulk($params)
     {
+        $this->validate($params, 'index,type,body');
+
         # Parse ElasticSearch\Client::bulk into es writer
         $offset = 0;
         while (isset($params['body'][$offset])) {
@@ -93,12 +93,33 @@ class EsWriterClient
             $op = array_keys($metadata)[0];
 
             $_params = array_merge([
-                'index' => $params['index'],
-                'type'  => $params['type'],
-                'body'  => $body,
+                'index'   => $params['index'],
+                'type'    => $params['type'],
+                'id'      => $metadata['_id'] ?? null,
+                'routing' => $metadata['_routing'] ?? null,
+                'body'    => $body,
             ], $metadata[$op]);
 
-            $this->{$op}($_params);
+            switch ($op) {
+                case 'index':
+                    $this->index($_params);
+                    break;
+
+                case 'create':
+                    $this->create($_params);
+                    break;
+
+                case 'update':
+                    $this->update($_params);
+                    break;
+
+                case 'delete':
+                    $this->delete($_params);
+                    break;
+
+                default:
+                    throw new RuntimeException('Unknown operator.');
+            }
 
             $offset += 2;
         }
